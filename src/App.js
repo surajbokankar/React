2d47cdaa04661d2628dd374418c4c5d98a8221af
surajbokankar <sbbokankar23@gmail.com>
2018-11-11 22:51:16 +0530

    Form Done

--- src/App.js
+++ src/App.js
@@ -9,38 +9,33 @@ import User from './component/user';
 class App extends Component {
 
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-     email:props.email,
-     password:props.password,
-    onLoginClick:this.onLoginClick.bind(this)
-        };
+    this.state = {email: '',password:''};
 
-        
-
-
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange=this.onPasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-
-  onEmailChange(event){
-    this.setState({
-      email:event.target.email
-    });
+  onEmailChange(event) {
+    this.setState({email: event.target.value});
   }
 
   onPasswordChange(event){
-    this.setState({
-      password:event.target.password
-    });
+    this.setState({password: event.target.value});
   }
 
-  onLoginClick(event){
+  handleSubmit(event) {
+    var emailVal=this.state.email;
+    var passVal=this.state.password;
+    if((emailVal!=null&&emailVal.length>0)&&(passVal!=null&&passVal.length>0)){
+      console.log("Email="+emailVal+"PassWord:"+passVal);
+    }else{
+      alert("Please fill details");
+    }
     
     event.preventDefault();
-    
-    console.log("Values="+event.target.email+"Password="+event.target.password);
-    
   }
 
 render() {
@@ -53,35 +48,80 @@ render() {
       width: "40%"
   }    
     
+
+  var button__container ={
+    margintop: '200px',
+    textalign: 'center'
+  }
+  var button ={
+    backgroundcolor:'green',
+    border: 'none',
+    color: 'white',
+    fontsize: 16,
+    height: '40px',
+    width: '200px'
+  }
+
+
     return (
 
-      <div  className="container">
+      
 
-       <form  onSubmit={this.onLoginClick}>
+      //  <form  onSubmit={this.onLoginClick.bind(this)}>
          
-         <label>Email</label>
-         <input  value={this.props.email}
-           onChange={this.onEmailChange.bind(this)
+      //    <label>Email</label>
+      //    <input  type="text" value={this.state.email}
+      //      onChange={this.onEmailChange
+      //    }/>
+
+      //    <br/>
+
+      //    <label>Password</label>
+      //    <input  type="text" value={this.state.password}
+      //      onChange={this.onPasswordChange
+      //    }/>
+
+      //    <br/>
+
+
+      //    <input type="submit" value="Submit"/>
+
+
+      //    </form>
+
+
+     
+
+ 
+      <form >
+      {/* <label>
+        Name:
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+        <input type="text" value={this.state.pass} onChange={this.onPasswordChange} />
+        </label> */}
+
+
+       <label>Email</label>
+         <input  type="text" value={this.state.email}
+           onChange={this.onEmailChange
          }/>
 
          <br/>
 
          <label>Password</label>
-         <input  value={this.props.password}
-           onChange={this.onPasswordChange.bind(this)
+         <input  type="text" value={this.state.password}
+           onChange={this.onPasswordChange
          }/>
 
          <br/>
 
+         <div className='button__container'>
+         <button className='button' onClick={this.handleSubmit}>Click Me</button>
+      </div>
+      
+    </form>
 
-         <button
-         type="submit"
-         >Submit</button>
-
-
-         </form>
- 
-        </div>
       
     );
   }
